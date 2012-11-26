dc2dad08f97c97af3ff5e7d4dd20ff16ca395dbd
Excon <excon@unknown00254b86b6f1.gateway.2wire.net>
2012-11-26 12:39:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weightyplates::Application.config.secret_token = '849f7fdde5c098551bfaddb588f2a1abd470d4ca7c7d695b178f9f60f81ab9d095f13e4b55d44eb059fd514cd667c2d436d4774ab0172bede2bc61001ca3cd7a'
