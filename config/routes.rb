b6e2aeec008aee0e4eca6ed2a0550b2b59f53f34
WilliamHuey <lliw1236@yahoo.com>
2012-11-28 14:19:20 -0800

    routes config

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Weightyplates::Application.routes.draw do
   devise_for :users
   resources :dashboard
 
+
+  namespace :user do
+    root :to => "dashboard#index"
+  end
+
   root to: 'home#index'
 
 end
