79a93b3083e8cd0be965ac100b1757472744705e
WilliamHuey <lliw1236@yahoo.com>
2012-11-27 17:38:37 -0800

    first three test on workout

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,4 @@
 class Workout < ActiveRecord::Base
   attr_accessible :id, :name, :note, :unit
+  validates_presence_of :name, :unit
 end
--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Workout do
+  before do
+    @workout = FactoryGirl.build(:workout)
+  end
+
+  subject { @workout }
+
+  it "should not be valid without a name" do
+    @workout.name = nil
+    @workout.save
+    @workout.should_not be_valid
+  end
+
+  it "should not be valid without a unit" do
+    @workout.unit = nil
+    @workout.save
+    @workout.should_not be_valid
+  end
+
+  it "should be valid without a note" do
+    @workout.note = nil
+    @workout.save
+    @workout.should be_valid
+  end
+
 
 end
