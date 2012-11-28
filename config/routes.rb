b6cfd829c252edb15ad597bcfc4177b317d472a9
WilliamHuey <lliw1236@yahoo.com>
2012-11-28 10:42:47 -0800

    routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Weightyplates::Application.routes.draw do
-  devise_for :users
+  #devise_for :users
+  #resources :users
 
-  get "dashboard/index"
+  devise_for :users,
+             :controllers => { :registrations => "users/registrations",
+                               :confirmations => "users/confirmations",
+                               :sessions => 'devise/sessions'},
+             :skip => [:sessions] do
+    get '/signin'   => "devise/sessions#new",       :as => :new_user_session
+    post '/signin'  => 'devise/sessions#create',    :as => :user_session
+    get '/signout'  => 'devise/sessions#destroy',   :as => :destroy_user_session
+    get "/signup"   => "user/registrations#new",   :as => :new_user_registration
+  end
 
-  root to: 'dashboard#index'
+  devise_scope :user do
+    #match "/some/route" => "some_devise_controller"
+    root to: 'devise/registrations#new'
+  end
+
+
+
+  #get "dashboard/index"
+  #root to: 'dashboard#index'
+
 end
