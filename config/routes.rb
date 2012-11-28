428af7668f74b19b70a484a3ca19bfafc427dbb9
WilliamHuey <lliw1236@yahoo.com>
2012-11-28 11:26:58 -0800

    routes for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,7 @@
 Weightyplates::Application.routes.draw do
-  #devise_for :users
-  #resources :users
+  devise_for :users
+  resources :dashboard
 
-  devise_for :users,
-             :controllers => { :registrations => "users/registrations",
-                               :confirmations => "users/confirmations",
-                               :sessions => 'devise/sessions'},
-             :skip => [:sessions] do
-    get '/signin'   => "devise/sessions#new",       :as => :new_user_session
-    post '/signin'  => 'devise/sessions#create',    :as => :user_session
-    get '/signout'  => 'devise/sessions#destroy',   :as => :destroy_user_session
-    get "/signup"   => "user/registrations#new",   :as => :new_user_registration
-  end
-
-  devise_scope :user do
-    #match "/some/route" => "some_devise_controller"
-    root to: 'devise/registrations#new'
-  end
-
-
-
-  #get "dashboard/index"
-  #root to: 'dashboard#index'
+  root to: 'home#index'
 
 end
