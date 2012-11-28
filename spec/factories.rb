c00dd64190e5e96f6deadd6597545b5db12ed6bf
WilliamHuey <lliw1236@yahoo.com>
2012-11-27 17:23:01 -0800

    factory setup

--- spec/factories.rb
+++ spec/factories.rb
@@ -10,4 +10,17 @@ FactoryGirl.define do
     type "resistance"
     sequence(:name) { |i| "Cat #{i}" }
   end
+
+  factory :workout do
+    name "workout name"
+    note "a note"
+    unit "kilograms"
+  end
+
+  factory :entry_detail do
+    set_number  1
+    reps        1
+    weight      150.5
+  end
+
 end
