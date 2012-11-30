88f348e5b360329e1f1ed23042415f64a565f6eb
WilliamHuey <lliw1236@yahoo.com>
2012-11-29 21:12:42 -0800

    unsuccessful login redirects to the referring page

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -1,13 +1,15 @@
 class CustomFailure < Devise::FailureApp
-  def redirect_url
-    root_path
+
+  def referer
+    @env['HTTP_REFERER'] || '/'
   end
 
   def respond
     if http_auth?
       http_auth
     else
-      redirect
+      redirect_to request.referer
     end
   end
+
 end
