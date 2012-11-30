07535d96dfc07e33d2db3f91f23b8a719a2729bd
WilliamHuey <lliw1236@yahoo.com>
2012-11-29 18:40:13 -0800

    attempt route changes unauthenticated redirected

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,8 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    #unless user_signed_in?
+    #  redirect_to :controller => 'home', :action => 'index'
+    #end
   end
 end
