b7a2ac7db3ad3016d09586e3f441e207bb08d131
Binh Tran <btbinhtran@gmail.com>
2012-12-01 17:46:21 -0800

    Separated login js from dashboard js. Also, cleaned up unnecessary sass and js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,3 @@
 //=require bootstrap-all
 //= require jquery
 //= require jquery_ujs
-//= require underscore
-//= require hamlcoffee
-//= require backbone
-//= require backbone.marionette
-//
-//= require_tree ./templates/
-//= require_tree .
