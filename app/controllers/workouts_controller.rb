d9d0cd02bccfdb68c38d98229104010f6b863f95
Binh Tran <btbinhtran@gmail.com>
2012-12-07 21:40:59 -0800

    Added api ability to delete workout. Fixed exercises_controller index bug from an ide reset

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,13 @@
 class WorkoutsController < ApplicationController
   respond_to :json
+  before_filter :authenticate_user!
+
 
   def index
-    respond_with(Workout.all)
+    respond_with(current_user.workouts)
+  end
+
+  def destroy
+    respond_with(current_user.workouts.destroy(params[:id]))
   end
 end
--- spec/controllers/workouts_controller_spec.rb
+++ spec/controllers/workouts_controller_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe WorkoutsController do
+  include Devise::TestHelpers
   let!(:application) { Doorkeeper::Application.create!(name: "BobApp", redirect_uri: "http://app.com") }
   let!(:user) { FactoryGirl.create(:user) }
   let!(:token) { Doorkeeper::AccessToken.create!(application_id: application.id, resource_owner_id: user.id) }
+  before do
+    sign_in user
+  end
 
   describe "index" do
     before do
@@ -20,5 +24,12 @@ describe WorkoutsController do
         workout.should == @workouts[i]
       end
     end
+  end
+
+  it "should be able to delete workout" do
+    workout = FactoryGirl.create(:workout, user_id: user.id)
+    expect do
+      delete :destroy, format: :json, id: workout.id
+    end.to change(user.workouts, :count).by(-1)
   end
 end
