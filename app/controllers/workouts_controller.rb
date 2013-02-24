2010a0e20da1fcd62e6ef67d1cb352bd0d3c730c
WilliamHuey <lliw1236@yahoo.com>
2013-02-23 23:03:30 -0800

    starting to save on second level of param

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -11,22 +11,63 @@ class WorkoutsController < ApplicationController
   end
 
   def create
-    models = %w(workout workout_entry entry_detail)
+    model_names = %w(workout workout_entry entry_detail)
     @workout = nil
-    workout_entries = nil
-    hash_ref = {:workout_entries => workout_entries}
-    def process_name(params, position, models, hash_ref)
+
+    def process_name(params, model_names, level, item_on, parent_level, parent_item)
+=begin
+      p "params"
+      p params
+      p "model names"
+      p model_names
+      p "level"
+      p level
+      p "item_on"
+      p item_on
+      p "parent level"
+      p parent_level
+      p "parent item"
+      p parent_item
+=end
+      current_item = model_names[level].pluralize(2)
+      nested_item = model_names[level + 1].pluralize(2)
+
+      if (level == 0) && (item_on == 0)
+        @workout = current_user.workouts.create((params["0"]).except(nested_item))
+        p 'created workout'
+        p "item last seen "
+        p item_on
+        process_name(params, model_names, level + 1, item_on, 0, 0)
+      else
+        p 'not in workout'
+        p current_item
+        p params["0"]
+        p params["0"][current_item]
+        p "item on is"
+
+        #@workout.send(current_item.to_sym).create(params["0"][current_item]["#{item_on}"])
+        current_entry = (params["0"][current_item]["#{item_on}"]).except(nested_item)
+        p current_entry
+        @workout.send(current_item.to_sym).create(current_entry)
+
+
+        #p @workout.send(current_item.to_sym)
+        #p params["0"][current_item]["#{item_on}"]
+        #p item_on
+      end
+
+
       #p "showing params"
       #p params
       #last_key = params.keys.last
       #p "last key"
-
+=begin
       p "position is"
       p position
 
       #p last_key
-      current_item = models[position].pluralize(2)
-      nested_item = models[position + 1].pluralize(2)
+      #current_item = models[position].pluralize(2)
+      #nested_item = models[position + 1].pluralize(2)
 
       #p "params are"
       #p (params["0"])
@@ -37,12 +78,13 @@ class WorkoutsController < ApplicationController
         p "workout creation started"
         #puts params["0"]
         #puts params["0"][nested_item]
-        process_name(params["0"][nested_item], 1, models, hash_ref)
+        process_name(params["0"][nested_item], 1, models)
       end
 
       if position != 0
         puts "after created workout"
-        puts params
+        puts params["0"]
+
         params.each do |k, v|
           p "iterating"
           #p current_item
@@ -51,21 +93,25 @@ class WorkoutsController < ApplicationController
           @workout.send(current_item.to_sym).create(v.except(nested_item))
           #p @workout.current_item.create(v.except(nested_item))
           #p v["workout_entries"].size
-          p @workout.workout_entries
-          p "nested item"
-          p v[nested_item]
+          #p @workout.workout_entries
+          #p "nested item"
+          #p v[nested_item]
+
+
+
           #size_of_nested = v[nested_item].size
           #process_name(v[nested_item], 1, models)
           #current_user.workouts.create(v.except("workout_entries"))
         end
-      end
 
+      end
+=end
 
     end
 
     #p models[1].pluralize(2)
-
-    process_name(params[:workout], 0, models, hash_ref)
+    #params, models, level, item_on, parent_level, parent_item
+    process_name(params[:workout], model_names, 0, 0, "n/a", "n/a")
     render :json => {:errors => @workout.errors.full_messages}, :status => 422
 =begin
     #creating the workout
