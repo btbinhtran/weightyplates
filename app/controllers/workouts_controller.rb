4576c24e68d08d93582f2040b73c9cab04dda170
WilliamHuey <lliw1236@yahoo.com>
2013-02-24 16:49:32 -0800

    redefined the string variable for fixnum error

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -14,58 +14,50 @@ class WorkoutsController < ApplicationController
     model_names = %w(workout workout_entry entry_detail)
     @workout = nil
 
-    def process_name(params, model_names, level, item_on, parent_level, parent_item, workout)
-=begin
-      p "params"
-      p params
-      p "model names"
-      p model_names
-      p "level"
-      p level
-      p "item_on"
-      p item_on
-      p "parent level"
-      p parent_level
-      p "parent item"
-      p parent_item
-=end
+    def process_name(params, model_names, level, item_on, parent_level, parent_item, workout, item_transversed)
+
       current_item = model_names[level].pluralize(2)
       if model_names[level + 1]
         nested_item = model_names[level + 1].pluralize(2)
       end
 
-
-      if (level == 0) && (item_on == 0)
+      if (level == 0) && (item_on == 0) && (@workout == nil)
         @workout = current_user.workouts.create((params["0"]).except(nested_item))
         p 'created workout'
+        p model_names[level + 1]
         parent_level = 0
-        process_name(params, model_names, level + 1, item_on, (parent_level + 1), 0, @workout)
+        parent_item = 0
+        process_name(params, model_names, level + 1, item_on, parent_level, parent_item, @workout, item_transversed.push(0))
       else
         p 'not in workout'
 
         #intermediate levels of nesting
         if model_names[level + 1]
+          #p "inter start"
+          #p current_item
+          #p params["0"][current_item]
           with_nested = params["0"][current_item]["#{item_on}"]
-          p 'stringify'
+          #p 'stringify'
 
           current_entry = with_nested.except(nested_item)
           #p current_entry
           @workout_entry = @workout.send(current_item.to_sym).create(current_entry)
-          p @workout
+          #p @workout
           workout = @workout_entry
-          p "the nested item is"
-          p nested_item
-          if nested_item
-            p "nested item"
-            p @workout[current_item]
-            process_name(params, model_names, level + 1, 0, (parent_level + 1), 0,  workout)
-          end
+
+          p "nested item"
+
+
+          process_name(params, model_names, (level + 1), item_on, (parent_level + 1), 0,  workout, item_transversed.push(item_on) )
+
         else
           #most inner level of nesting
           if !model_names[level + 1]
             p 'most inner level'
             p current_item
             p  workout
+            p "item transverse level"
+            p item_transversed
             #@workout.send(current_item.to_sym).create(current_entry)
             #p params["0"][current_item]
             #p params["0"][current_item].size
@@ -83,7 +75,7 @@ class WorkoutsController < ApplicationController
 
     #p models[1].pluralize(2)
     #params, models, level, item_on, parent_level, parent_item
-    process_name(params[:workout], model_names, 0, 0, "n/a", "n/a", @workout)
+    process_name(params[:workout], model_names, 0, 0, "n/a", "n/a", @workout, %w())
     render :json => {:errors => @workout.errors.full_messages}, :status => 422
 =begin
     #creating the workout
