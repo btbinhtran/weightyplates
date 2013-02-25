d1b7360118216a409c8daea58332f2902199103e
WilliamHuey <lliw1236@yahoo.com>
2013-02-24 22:06:43 -0800

    another try to get accepts nested attributes to work again

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,10 +1,12 @@
 class Workout < ActiveRecord::Base
 
   has_many :workout_entries, dependent: :destroy, :inverse_of => :workout
-  attr_accessible :id, :name, :note, :unit, :workout_entries
+  attr_accessible :id, :name, :note, :unit, :workout_entries_attributes, :workout_entries
   belongs_to :user
 
-  accepts_nested_attributes_for :workout_entries
+
+  accepts_nested_attributes_for :workout_entries,  :allow_destroy => true
+  accepts_nested_attributes_for :user
 
   validates_presence_of :name #, :with => /\A\Z/
   validates :unit, :presence => true, :inclusion => {:in => %w(kg lb)}
